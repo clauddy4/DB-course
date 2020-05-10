ddea1fc20733f99388683da41ea2fab02cf6d666
Olga Makarova <53290930+clauddy4@users.noreply.github.com>
2020-05-10 10:18:01 +0300

    Update lab3.sql

--- lab3/lab3.sql
+++ lab3/lab3.sql
@@ -4,50 +4,50 @@ use bd_course;
 -- 1.1. Без указания списка полей
    INSERT INTO employee VALUES ('1', 'Andrey', 'Tokarev', 'QA engineer', 'tokarinze@gmail.com' );
 
---  1.2. Ñ óêàçàíèåì ñïèñêà ïîëåé
+--  1.2. С указанием списка полей
   	INSERT INTO project (title, cost, start_date, ending_date) VALUES ('BD course', '1000000000', '11-12-20', '12-10-28');
 
---  3. Ñ ÷òåíèåì çíà÷åíèÿ èç äðóãîé òàáëèöû
+--  3. С чтением значения из другой таблицы
   	INSERT INTO unemployed (first_name, last_name, position) SELECT first_name, last_name, position FROM employee;
 
 -- 2. DELETE
--- 1. Âñåõ çàïèñåé
+-- 1. Всех записей
    DELETE unemployed;
--- 2. Ïî óñëîâèþ
+-- 2. По условию
    DELETE FROM unemployed WHERE first_name = 'Andrey';
--- 3. Î÷èñòèòü òàáëèöó
+-- 3. Очистить таблицу
    TRUNCATE TABLE unemployed;
 
 -- 3. UPDATE
--- 1. Âñåõ çàïèñåé
+-- 1. Всех записей
    UPDATE employee SET position = 'fired';
--- 2. Ïî óñëîâèþ îáíîâëÿÿ îäèí àòðèáóò
+-- 2. По условию обновляя один атрибут
    UPDATE employee SET position = 'Middle developer' WHERE position = 'Junior developer';
--- 3. Ïî óñëîâèþ îáíîâëÿÿ íåñêîëüêî àòðèáóòîâ
+-- 3. По условию обновляя несколько атрибутов
    UPDATE employee SET position = 'Rockstar', email='johnyrockstar@gmail.com' WHERE first_name = 'John' AND last_name='Dohn';
 
 -- 4. SELECT
---	1. Ñ îïðåäåëåííûì íàáîðîì èçâëåêàåìûõ àòðèáóòîâ (SELECT atr1, atr2 FROM...)
+--	1. С определенным набором извлекаемых атрибутов (SELECT atr1, atr2 FROM...)
 	SELECT first_name, last_name, position, email FROM employee;
---	2. Ñî âñåìè àòðèáóòàìè (SELECT * FROM...)
+--	2. Со всеми атрибутами (SELECT * FROM...)
 	SELECT * FROM project;
---	3. Ñ óñëîâèåì ïî àòðèáóòó (SELECT * FROM ... WHERE atr1 = "")
+--	3. С условием по атрибуту (SELECT * FROM ... WHERE atr1 = "")
 	SELECT * FROM project WHERE cost < 100000000;
 
-	-- 5. SELECT ORDER BY + TOP (LIMIT)
---  1. Ñ ñîðòèðîâêîé ïî âîçðàñòàíèþ ASC + îãðàíè÷åíèå âûâîäà êîëè÷åñòâà çàïèñåé
+-- 5. SELECT ORDER BY + TOP (LIMIT)
+--  1.  С сортировкой по возрастанию ASC + ограничение вывода количества записей
 	SELECT TOP 5 * FROM project ORDER BY cost ASC;
---  2. Ñ ñîðòèðîâêîé ïî óáûâàíèþ DESC
+--  2. С сортировкой по убыванию DESC
 	SELECT TOP 5 * FROM employee ORDER BY last_name DESC;
---  3. Ñ ñîðòèðîâêîé ïî äâóì àòðèáóòàì + îãðàíè÷åíèå âûâîäà êîëè÷åñòâà çàïèñåé
+--  3. С сортировкой по двум атрибутам + ограничение вывода количества записей
 	SELECT TOP 5 * FROM employee ORDER BY first_name, last_name DESC;
---  4. Ñ ñîðòèðîâêîé ïî ïåðâîìó àòðèáóòó, èç ñïèñêà èçâëåêàåìûõ
+--  4. С сортировкой по первому атрибуту, из списка извлекаемых
 	SELECT TOP 5 * FROM employee ORDER BY 1;
 
--- 6. Ðàáîòà ñ äàòàìè. Íåîáõîäèìî, ÷òîáû îäíà èç òàáëèö ñîäåðæàëà àòðèáóò ñ òèïîì DATETIME.
---  1. WHERE ïî äàòå
+-- 6. Работа с датами. Необходимо, чтобы одна из таблиц содержала атрибут с типом DATETIME.
+--  1. WHERE по дате
 	SELECT * FROM project WHERE start_date = '19/09/2012 00:00:00';
---  2. Èçâëå÷ü èç òàáëèöû íå âñþ äàòó, à òîëüêî ãîä. Íàïðèìåð, ãîä ðîæäåíèÿ àâòîðà.
+--  2. Извлечь из таблицы не всю дату, а только год. Например, год рождения автора.
 	SELECT title, YEAR(start_date) AS start_date FROM project;
 
 	-- 7. SELECT GROUP BY ñ ôóíêöèÿìè àãðåãàöèè
@@ -63,17 +63,17 @@ use bd_course;
 	SELECT title, COUNT(title) AS count FROM project GROUP BY title;
 
 -- 8. SELECT GROUP BY + HAVING
---  1. Íàïèñàòü 3 ðàçíûõ çàïðîñà ñ èñïîëüçîâàíèåì GROUP BY + HAVING
+--      1. Написать 3 разных запроса с использованием GROUP BY + HAVING
 	SELECT start_date FROM project GROUP BY start_date HAVING start_date < '19/09/2020 00:00:00';
 	SELECT title FROM project GROUP BY title HAVING MAX(cost) > 1000000;
 	SELECT title FROM project GROUP BY title HAVING COUNT(title) > 1;
 
 -- 9. SELECT JOIN
---  1. LEFT JOIN äâóõ òàáëèö è WHERE ïî îäíîìó èç àòðèáóòîâ
+--  1. LEFT JOIN двух таблиц и WHERE по одному из атрибутов
 	SELECT * FROM employee LEFT JOIN project_participation ON employee.id_employee = project_participation.id_employee WHERE employee.id_employee > 2;
---  2. RIGHT JOIN. Ïîëó÷èòü òàêóþ æå âûáîðêó, êàê è â 9.1
+--  2. RIGHT JOIN. Получить такую же выборку, как и в 9.1
 	SELECT * FROM project_participation RIGHT JOIN employee ON employee.id_employee = project_participation.id_employee WHERE employee.id_employee > 2;
---  3. LEFT JOIN òðåõ òàáëèö + WHERE ïî àòðèáóòó èç êàæäîé òàáëèöû
+--  3. LEFT JOIN трех таблиц + WHERE по атрибуту из каждой таблицы
 	SELECT 
 		employee.id_employee, employee.last_name, 
 		project_participation.id_project, project_participation.id_employee, 
@@ -82,13 +82,13 @@ use bd_course;
 		employee LEFT JOIN project_participation ON employee.id_employee = project_participation.id_employee
     LEFT JOIN project ON project.id_project = project_participation.id_project
     WHERE employee.last_name = 'Holmes' AND employee.id_employee > 2 AND project.cost > 10000;
---  4. FULL OUTER JOIN äâóõ òàáëèö
+--  4. FULL OUTER JOIN двух таблиц
 	SELECT * FROM project FULL OUTER JOIN employee ON project.id_project = employee.id_employee;
 
--- 10. Ïîäçàïðîñû
---  1. Íàïèñàòü çàïðîñ ñ WHERE IN (ïîäçàïðîñ)
+-- 10. Подзапросы
+-- 1. Написать запрос с WHERE IN (подзапрос)
 	SELECT * FROM project WHERE cost IN ('100000', '1000000');
-	--  2. Íàïèñàòü çàïðîñ SELECT atr1, atr2, (ïîäçàïðîñ) FROM ...    
+--2. Написать запрос SELECT atr1, atr2, (подзапрос) FROM   
 
 	SELECT title, cost FROM project where start_date in('12/11/2020', '16/03/2030');
 
